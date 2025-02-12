7c4a034cdae7d9957252fff8fd544796d3fc936b
Milind Mishra <milind.mishra4@gmail.com>
2025-02-12 16:01:00 +0530

    init sidepanel ext

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/manifest.json",
+          dest: ".",
+        },
+        {
+          src: "public/background.js",
+          dest: ".",
+        },
+      ],
+    }),
+  ],
+  build: {
+    outDir: "build",
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+    },
+  },
+});
